eed3dadeb8f6a64c77ed80e1c7dc4d5e3a168c7e
Marc Bachmann <marc.brookman@gmail.com>
2015-09-12 15:10:05 +0100

    Fix test where environment variable was used for expected value instead the one from file

--- test/index.js
+++ test/index.js
@@ -2,6 +2,10 @@ var assert = require('assert')
 var assertNodeVersion = require('../')
 var path = require('path')
 
+// Delete environment variable which would prevent a correct test
+// Without this the test would have a wrong 'expected' value
+delete process.env.npm_package_engines_node
+
 assert.throws(function () {
   assertNodeVersion(path.join(__dirname, 'example'))
 }, /Expected: 'v0\.6\.0'/)
