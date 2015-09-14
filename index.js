ef10309ee662eacba4a0e92fbec1717dd67df093
Marc Bachmann <marc.brookman@gmail.com>
2015-09-14 10:32:35 +0200

    Fix range assertions

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = function assertNodeVersion (dir) {
   var version = require('expected-node-version')(dir)
   if (version) {
-    var satisfied = require('semver').satisfies(version, process.version)
+    var satisfied = require('semver').satisfies(process.version, version)
     if (!satisfied) {
       throw new Error("Failed to satisfy expected node version. Expected: '" + version + "', Current '" + process.version + "'")
     }
--- test/index.js
+++ test/index.js
@@ -2,10 +2,22 @@ var assert = require('assert')
 var assertNodeVersion = require('../')
 var path = require('path')
 
+// Test assertion using file
+//
 // Delete environment variable which would prevent a correct test
 // Without this the test would have a wrong 'expected' value
 delete process.env.npm_package_engines_node
-
 assert.throws(function () {
   assertNodeVersion(path.join(__dirname, 'example'))
 }, /Expected: 'v0\.6\.0'/)
+
+// Test range assertions
+// Version range
+process.env.npm_package_engines_node = '0.4.0 - 0.6.0'
+assert.throws(function () {
+  assertNodeVersion(path.join(__dirname, 'example'))
+}, /Expected: '0.4.0 - 0.6.0'/)
+
+// Larger than
+process.env.npm_package_engines_node = '>=0.10.0'
+assertNodeVersion(path.join(__dirname, 'example'))
